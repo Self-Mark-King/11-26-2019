71c10ac5f034289b5e0f7c308df6008a46b630e6
Self-Mark-King <mg_king@hotmail.com>
2019-11-26 21:04:09 +0000

    add to serializers.py, urls.py and views.py

--- app/drf/views.py
+++ app/drf/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from drf.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+class User_viewset(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all().order_by('date_joined')
+    serializer_class = UserSerializer
+
+class Group_Viewset(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
